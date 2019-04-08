e745dcba10cdbcd50803b1716a02bce280ac4e8a
200202 <james.j.lee@cubic.com>
2019-04-08 13:01:26 +1000

    Revert "Added selected S128 workaround scripts"
    This reverts commit d4dcd98468a446a2bfc8b9f28876bdb4e2cc869e.

--- Stage 128 Workaround scripts/A_Arrears (W5).sql
+++
@@ -1,9 +0,0 @@
-select 
-    transit_account_id
-    ,activity_dtm+11/24
-    ,current_balance
-from abp_main.transit_account 
-where current_balance < 0 
-and account_status_id = 1 
-and activity_dtm+11/24 < trunc(sysdate,'DD')+4/11 
-;
