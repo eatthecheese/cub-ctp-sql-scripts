7e15f95bd5cdd49b01eab58efa8c5dd6761c413d
200202 <james.j.lee@cubic.com>
2019-09-27 15:14:47 +1000

    Updated Stage 105 MV refresh script for correct prod implementation

--- Stage_105_scripts/fare_sheet_mv_refresh.sql
+++ Stage_105_scripts/fare_sheet_mv_refresh.sql
@@ -4,7 +4,7 @@ BEGIN DBMS_SNAPSHOT.REFRESH( '"ABP_MAIN"."FARE_RULE_IN_USE"'); end;
 BEGIN DBMS_SNAPSHOT.REFRESH( '"ABP_MAIN"."FARE_RULE_X_CONC_LEVEL_IN_USE"'); end;
 
 /* Part 2: Update the Max Fare Rule Effective Datetime in SYS_INFO */
-/* Update the time below such that the effective datetime matches that of the CSELR go-live for CTP*/
+/* CSELR fare sheet will be made effective on 9AM 2019 OCT 10*/
 update ABP_MAIN.SYS_INFO
-set MAX_FARE_RULE_EFFECTIVE_DTM  = TO_TIMESTAMP('2019-12-05 04:00:00.000000', 'YYYY-MM-DD HH24:MI:SS.FF6') -10/24
+set MAX_FARE_RULE_EFFECTIVE_DTM  = TO_TIMESTAMP('2019-10-10 09:00:00.000000', 'YYYY-MM-DD HH24:MI:SS.FF6') -10/24
 ;
